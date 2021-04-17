5e12a6b28ac4b1ae4b06bcddf9f46d00c22241c6
longlou123 <448225728@qq.com>
2021-04-17 15:23:55 +0800

    http请求

---
+++ oneWeek/parser.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: dalou
+ * @Date: 2021-04-17 15:10:59
+ * @LastEditTime: 2021-04-17 15:12:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /jike-study/oneWeek/parser.js
+ */
+module.exports.parseHTML = function parseHTML(html) {
+  console.log(html)
+}
--- oneWeek/serve.js
+++ oneWeek/serve.js
@@ -1,19 +1,27 @@
+/*
+ * @Author: dalou
+ * @Date: 2021-04-16 18:49:55
+ * @LastEditTime: 2021-04-17 15:13:03
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /jike-study/oneWeek/serve.js
+ */
 const http = require("http")
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
 	let body = []
-	req.on('err', (err) =>{
+	req.on('err', (err) => {
 		console.log(err)
-	}).on('data', (chunk) =>{
-		body.push(chunk.toString())
-	}).on('end', ()=>{
+	}).on('data', (chunk) => {
+		body.push(chunk)
+	}).on('end', () => {
 		body = Buffer.concat(body).toString()
 		console.log(body)
-		res.writeHead(200, {'Content-Type': 'text/plain'})
+		res.writeHead(200, { 'Content-Type': 'text/plain' })
 		// res.setHeader('X-FOO', 'bar')
 		// res.setHeader('Content-Type', 'text/html')
 		res.end(' Hello world\n')
-	})	
+	})
 }).listen(8080)
 
 console.log('serve start')
